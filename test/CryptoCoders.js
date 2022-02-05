90c1ad6ead3668c2dba782da5395368003a1adcb
Rakibul71 <rakibulislam202019@gmail.com>
2022-02-06 00:23:58 +0600

    crypto coder solidity test

--- test/CryptoCoders.js
+++ test/CryptoCoders.js
@@ -1,8 +1,17 @@
 const CryptoCoders = artifacts.require("./CryptoCoders.sol");
 
 contract("CryptoCoders", (accounts) => {
+  let contract;
+  before(async () => {
+    contract = await CryptoCoders.deployed();
+  });
   it("....get deployed", async () => {
-    const contract = await CryptoCoders.deployed();
-    console.log(contract);
+    assert.notEqual(contract, "");
+  });
+  it("...get's minted and added", async () => {
+    const result = await contract.mint("Rakibul");
+    let coder = await contract.coders(0);
+    // console.log(coder);
+    assert(coder, "Rakibul");
   });
 });
