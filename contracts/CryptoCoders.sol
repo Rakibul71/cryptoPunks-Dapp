110f7106075a6fb1d03f5d9c6df4958979f38635
Rakibul71 <rakibulislam202019@gmail.com>
2022-02-06 20:06:36 +0600

    complete solidity part

--- contracts/CryptoCoders.sol
+++ contracts/CryptoCoders.sol
@@ -6,12 +6,16 @@ import "../client/node_modules/@openzeppelin/contracts/token/ERC721/extensions/E
 contract CryptoCoders is ERC721, ERC721Enumerable {
     string[] public coders;
 
+    mapping(string => bool) _coderExists;
+
     constructor() ERC721("CryptoCoders", "CSS") {}
 
     function mint(string memory coder) public {
+        require(!_coderExists[coder]);
         coders.push(coder);
         uint256 _id = coders.length - 1;
         _mint(msg.sender, _id);
+        _coderExists[coder] = true;
     }
 
     function _beforeTokenTransfer(
