17c13d1021fb412cc72a95cb370d4639ae61a9c9
Rakibul71 <rakibulislam202019@gmail.com>
2022-02-06 23:28:51 +0600

    final commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,73 +1,124 @@
-import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import React, { useState, useEffect } from "react";
+import CryptoCoder from "./contracts/CryptoCoders.json";
+
 import getWeb3 from "./getWeb3";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./App.css";
 
-class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+const App = () => {
+  const [account, setAccount] = useState("");
+  const [contract, setContract] = useState("");
+  const [mintText, setMintText] = useState("");
+  const [coders, setCoders] = useState([]);
 
-  componentDidMount = async () => {
-    try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
+  const mint = () => {
+    contract.methods.mint(mintText).send({ from: account }, (error) => {
+      console.log("it worked");
+      if (!error) {
+        setCoders([...coders, mintText]);
+        setMintText("");
+      }
+    });
+  };
 
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
+  const loadNFTS = async (contract) => {
+    const totalSupply = await contract.methods.totalSupply().call();
+    console.log(totalSupply);
+    console.log("trying again");
+    let results = [];
+    for (let i = 0; i < totalSupply; i++) {
+      let coder = await contract.methods.coders(i).call();
+      results.push(coder);
+    }
+    setCoders(results);
+  };
 
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
-
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
-      console.error(error);
+  const loadWeb3Account = async (web3) => {
+    const accounts = await web3.eth.getAccounts();
+    if (accounts) {
+      setAccount(accounts[0]);
     }
   };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
+  const loadWeb3Contract = async (web3) => {
+    const networkId = await web3.eth.net.getId();
+    const networkData = CryptoCoder.networks[networkId];
+    if (networkData) {
+      const abi = CryptoCoder.abi;
+      const address = networkData.address;
+      const contract = new web3.eth.Contract(abi, address);
+      setContract(contract);
+      return contract;
+    }
   };
 
-  render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
-    return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+  useEffect(async () => {
+    const web3 = await getWeb3();
+    await loadWeb3Account(web3);
+    const contract = await loadWeb3Contract(web3);
+    await loadNFTS(contract);
+  }, []);
+
+  return (
+    <div>
+      <nav className="navbar navbar-light bg-light px-4">
+        <a className="navbar-brand" href="#">
+          Crypto Coders
+        </a>
+        <span>{account}</span>
+      </nav>
+      <div className="container-fluid mt-5">
+        <div className="row">
+          <div className="col d-flex flex-column align-items-center">
+            <img
+              className="mb-4"
+              src="https://avatars.dicebear.com/api/pixel-art/naz.svg"
+              alt=""
+              width="72"
+            />
+            <h1 className="display-5 fw-bold">Crypto Coders</h1>
+            <div className="col-6 text-center mb-3">
+              <p className="lead text-center">
+                These are some of the most highly motivated coders in the world!
+                We are here to learn coding and apply it to the betterment of
+                humanity. We are inventors, innovators, and creators
+              </p>
+              <div>
+                <input
+                  type="text"
+                  value={mintText}
+                  onChange={(e) => setMintText(e.target.value)}
+                  className="form-control mb-2"
+                  placeholder="e.g. Rakib"
+                />
+                <button onClick={mint} className="btn btn-primary">
+                  Mint
+                </button>
+              </div>
+            </div>
+            <div className="col-8 d-flex justify-content-center flex-wrap">
+              {coders.map((coder, key) => (
+                <div
+                  className="d-flex flex-column align-items-center"
+                  key={key}
+                >
+                  <img
+                    width="150"
+                    src={`https://avatars.dicebear.com/api/pixel-art/${coder.replace(
+                      "#",
+                      ""
+                    )}.svg`}
+                  />
+                  <span>{coder}</span>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
